bf541330241af5ed1eebd054ac6f74b6ff55a396
musat023 <145437556+musat023@users.noreply.github.com>
2026-03-06 09:14:14 +0100

    Add city data and chart history constants

---
+++ City Simulation/js/utils/constans.js
@@ -0,0 +1,15 @@
+export const CITY_DATA = {
+    name: "Zapizor",
+    population: 120000,
+    budget: 5000000,
+    energy: 75,
+    happiness: 68
+};
+
+export const CHART_HISTORY = [
+    110000,
+    112000,
+    115000,
+    118000,
+    120000
+];
