372e25aa6b59e904e5943f501f673c45aa06d9e7
musat023 <145437556+musat023@users.noreply.github.com>
2026-03-06 09:15:37 +0100

    Refactor script.js to initialize dashboard and notifications

--- City Simulation/js/script.js
+++ City Simulation/js/script.js
@@ -1,3 +1,15 @@
+import { CITY_DATA } from "./constants.js";
+import { Logger } from "./logger.js";
+import { initDashboard } from "../ui/dashboard.js";
+import { initChart } from "../ui/charts.js";
+import { initNotifications } from "../ui/notifications.js";
+import { initAnimations } from "../ui/animation.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-    Terminal.init();
+    Logger.info("Aplikace spuštěna");
+
+    initDashboard(CITY_DATA);
+    initChart();
+    initNotifications();
+    initAnimations();
 });
